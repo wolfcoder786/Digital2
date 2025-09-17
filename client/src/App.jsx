8dbe1161fff1e6151c02a294e2540228d0548b9e
wolfcoder786 <155200883+wolfcoder786@users.noreply.github.com>
2025-09-17 19:14:17 +0530

    bugs
    hi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Features from "./components/Features";
+import Features from "./components/features";
 
 function App() {
   const [currentPage, setCurrentPage] = useState("home");
